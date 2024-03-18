f36efc06b49c49670ad77f1fa302b376cdafbcc0
thecomediandan <danishpurplesnake@gmail.com>
2024-03-18 00:00:21 -0400

    Primera subida de inicio de los apuntes sobre Git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'apuntes-git-angular';
+  title = 'Estos son mis Apuntes de Git :D';
 }
